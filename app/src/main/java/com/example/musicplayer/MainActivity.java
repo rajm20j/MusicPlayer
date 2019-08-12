b1bcb3076babef2d8b18d84cc2c3103996e856e7
Mayank <rajm20j@gmail.com>
2019-08-13 00:16:14 +0530

    initial

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.musicplayer;
+
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int REQUEST_PERMISSIONS = 12345;
+
+    private static final int PERMISSIONS_COUNT = 1;
+
+    private boolean arePermissionsDenied(){
+        for(int i=0;i<PERMISSIONS_COUNT;i++)
+        {
+            if(checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(arePermissionsDenied()){
+            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+            recreate();
+        }else{
+            onResume();
+        }
+    }
+
+    private boolean isMusicPlayerInit;
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()){
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
+
+        if(!isMusicPlayerInit){
+            final ListView listView=findViewById(R.id.listView);
+
+
+            isMusicPlayerInit=true;
+        }
+    }
+
+    class TextAdapter extends BaseAdapter{
+
+        private List<String> data= new ArrayList<>();
+
+        void setData(List<String> mData){
+            data.clear();
+            data.addAll(mData);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount(){
+            return data.size();
+        }
+
+        @Override
+        public String getItem(int position){
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position){
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            if(convertView == null){
+            }
+
+        }
+    }
+}
