a5041ad83aba8a50853f9f93c253efad9d5bf778
Mayank <rajm20j@gmail.com>
2019-08-14 00:18:53 +0530

    initial

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.musicplayer;
 import android.Manifest;
 import android.app.ActivityManager;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +11,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.io.File;
@@ -58,22 +62,22 @@ public class MainActivity extends AppCompatActivity {
 
     private List<String> musicFilesList;
 
-    private void addMusicFilesFrom(String dirPath){
+    private void addMusicFilesFrom(String dirPath) {
         final File musicDir = new File(dirPath);
-        if(!musicDir.exists()){
+        if (!musicDir.exists()) {
             musicDir.mkdir();
             return;
         }
         final File[] files = musicDir.listFiles();
-        for(File file: files){
-            final String path=file.getAbsolutePath();
-            if(path.endsWith(".mp3")){
+        for (File file : files) {
+            final String path = file.getAbsolutePath();
+            if (path.endsWith(".mp3")) {
                 musicFilesList.add(path);
             }
         }
     }
 
-    private void fillMusicList(){
+    private void fillMusicList() {
         musicFilesList.clear();
         addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_MUSIC
@@ -86,6 +90,22 @@ public class MainActivity extends AppCompatActivity {
         )));
     }
 
+    private MediaPlayer mp;
+
+    private int playMusicFile(String path) {
+        mp = new MediaPlayer();
+        try {
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mp.getDuration();
+    }
+
+    private int songPosition;
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -96,13 +116,80 @@ public class MainActivity extends AppCompatActivity {
 
         if (!isMusicPlayerInit) {
             final ListView listView = findViewById(R.id.listView);
-            final TextAdapter textAdapter=new TextAdapter();
-            musicFilesList=new ArrayList<>();
+            final TextAdapter textAdapter = new TextAdapter();
+            musicFilesList = new ArrayList<>();
             fillMusicList();
             textAdapter.setData(musicFilesList);
             listView.setAdapter(textAdapter);
+            final SeekBar seekBar = findViewById(R.id.seekBar);
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                int songProgress;
 
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    songProgress = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    mp.seekTo(songProgress);
+                }
+            });
+
+            final TextView songPositionView=findViewById(R.id.currentPosition);
+            final TextView songDurationView=findViewById(R.id.songDuration);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    final String musicFilePath = musicFilesList.get(position);
+                    final int songDuration = playMusicFile(musicFilePath)/1000;
+                    seekBar.setMax(songDuration);
+                    seekBar.setVisibility(View.VISIBLE);
+                    songDurationView.setText(String.valueOf(songDuration/60)+":"+String.valueOf(songDuration%60));
+                    new Thread() {
+                        public void run() {
+                            songPosition = 0;
+                            while (songPosition < songDuration) {
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                songPosition+=1;
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        seekBar.setProgress(songPosition);
+                                        songPositionView.setText(String.valueOf(songPosition));
+                                    }
+                                });
+                            }
+                        }
+                    }.start();
+                }
+            });
             isMusicPlayerInit = true;
+
+            final Button pauseButton= findViewById(R.id.pauseButton);
+            pauseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mp.isPlaying())
+                    {
+                        mp.pause();
+                    }
+                    else
+                    {
+                        mp.start();
+                    }
+                }
+            });
         }
     }
 
